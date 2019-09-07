b8c14b3a4c27d841b818b92149896787ce52184b
OFFICE\avi.y <avi.y@tabooa.com>
2019-09-07 12:48:03 +0300

    - minor change

--- src/components/App.js
+++ src/components/App.js
@@ -12,25 +12,25 @@ class App extends React.Component {
       {lat: "a", lng: "eee", id: 8378}
     ],
     editMark: null
-    // editMark: {lat: "a", lng: "eee", id: 8378}
   }
 
   onFormSubmit = formData => {
-    if (formData.id) {
+    console.log('formData to submit: ', formData)
+    if (formData.id) { // edit mark
       let marks = [...this.state.marks]
       for (let i = 0 ; i < marks.length ; ++i) {
         if (marks[i].id === formData.id) {
           marks[i] = {...formData}
         }
       }
+      console.log('new marks state after Edit mark: ', [...marks])
       this.setState({
         marks: [...marks],
         editMark: null
       })
-    } else {
-      console.log(formData)
+    } else { // add mark
       formData.id = uniqueId();
-      console.log([...this.state.marks, formData])
+      console.log('new marks state after Add mark: ', [...this.state.marks, formData])
       this.setState({
         marks: [...this.state.marks, formData],
         editMark: null
